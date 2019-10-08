7eaf356f4e6edd9c6979f996286d210d92fd7101
Doggy1998 <44040901+Doggy1998@users.noreply.github.com>
2019-10-08 22:15:40 +0800

    第三個作業1008

---
+++ math_1008_hw3.py
@@ -0,0 +1,13 @@
+# {(月薪-月支出)*12個月+(月薪-月支出)利率}是否>=1000000
+import math
+while True:
+    wage = input("想知道工作幾年有100萬，請輸入預期月薪: ")
+    if "不" in wage:
+        print("好吧那不吵你了，我要去賺錢了，掰掰")
+        break
+    else:
+        wage = float(wage)
+    cost = float(input("請輸入預期月支出: "))
+
+    year = math.ceil(1000000/(wage-cost))
+    print("需要 %d 年喔\n" % (year, ))
